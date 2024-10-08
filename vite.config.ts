60689678288e5b92ce0dfc46f966c9bb92eba904
TruffleHogDev <114633163+TruffleHogDev@users.noreply.github.com>
2024-10-08 01:49:10 -0600

    Update vite.config.ts
    Reversion with minor update to serverBuildFile

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -7,15 +8,12 @@ export default defineConfig({
       basename: "/",
       buildDirectory: "build",
       future: {
-        /* any enabled future flags */
-      },
-      ignoredRouteFiles: ["**/*.css"],
-      routes(defineRoutes) {
-        return defineRoutes((route) => {
-          route("/somewhere/cool/*", "catchall.tsx");
-        });
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
       },
       serverBuildFile: "index.js",
     }),
+    tsconfigPaths(),
   ],
 });
