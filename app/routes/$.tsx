62ae0581c3c0b525d78a0ef4584bac0d3c1ee575
TruffleHogDev <114633163+TruffleHogDev@users.noreply.github.com>
2025-01-04 16:25:10 -0700

    Update $.tsx
    updating redirect

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -3,7 +3,7 @@ import { redirect } from "@remix-run/node";
 
 export const loader = async () => {
   // Redirect to _index.tsx
-  return redirect("/"); // Adjust the path as necessary
+  return redirect("/app/routes/_index.tsx"); // Adjust the path as necessary
 };
 
 export default function CatchAllRoute() {
