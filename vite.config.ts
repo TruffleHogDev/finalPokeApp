a6ca4dcc70371a367611c7f1c222e00dc7533059
TruffleHogDev <114633163+TruffleHogDev@users.noreply.github.com>
2025-01-04 17:26:19 -0700

    Update vite.config.ts
    updated serverBuildFile again to prevent nesting issues.

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
-      serverBuildFile: "build/server/index.js",
+      serverBuildFile: "index.js",
     }),
     tsconfigPaths(),
   ],
