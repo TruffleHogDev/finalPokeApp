f881574c41ac7d3423473e931799e66e666e240f
TruffleHogDev <114633163+TruffleHogDev@users.noreply.github.com>
2025-05-19 02:40:07 -0600

    Gen 4 update
    Added gen 4, updated SearchCategory for missing gen 3 entries

--- app/utils/PredictiveSearch.tsx
+++ app/utils/PredictiveSearch.tsx
@@ -2,18 +2,25 @@
 
 import { genOnePokemon } from "../data/GenOnePokemon";
 import { genTwoPokemon } from "../data/GenTwoPokemon";
-import { genThreePokemon } from "~/data/GenThreePokemon";
+import { genThreePokemon } from "../data/GenThreePokemon";
+import { genFourPokemon } from "~/data/GenFourPokemon";
 // Future additions:
 // import { regionalForms } from "../data/RegionalForms";
 // import { megaEvolutions } from "../data/MegaEvolutions";
 
-type SearchCategory = "gen1" | "gen2" | "all"; // Extend this list as needed
+type SearchCategory = "gen1" | "gen2" | "gen3" | "gen4" | "all"; // Extend this list as needed
 
 const categoryMap: Record<SearchCategory, string[]> = {
   gen1: genOnePokemon,
   gen2: genTwoPokemon,
   gen3: genThreePokemon,
-  all: [...genOnePokemon, ...genTwoPokemon, ...genThreePokemon], // Add more sources here as needed
+  gen4: genFourPokemon,
+  all: [
+    ...genOnePokemon,
+    ...genTwoPokemon,
+    ...genThreePokemon,
+    ...genFourPokemon,
+  ], // Add more sources here as needed
 };
 
 /**
